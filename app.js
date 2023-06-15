3404c228bf622c55999e8aaffeee3a29fda52b9a
Damola Olusakin <olusakinadedamola@gmail.com>
2023-06-15 12:56:29 -0400

    created the rendeer board function

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
 /*----- constants -----*/
-
-
+const COLORS = {
+  '0': 'white',
+  '1': 'purple',
+  '-1': 'orange'
+}
 /*----- state variables -----*/
 
-
-let board
-let winner
-let turn
-
+let board;
+let winner;
+let turn;
 
 /*----- cached elements  -----*/
-
+const messageEl
 
 /*----- event listeners -----*/
 
-
 /*----- functions -----*/
-function init(){
+function init() {
   board = [
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-  ]
-  turn = 1
-  winner = null
-  render()
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+  ];
+  turn = 1;
+  winner = null;
+  render();
 }
 
-init()
+init();
 
-function render(){
-  renderBoard()
-  renderMessage()
-  renderControls()
+function render() {
+  renderBoard();
+  renderMessage();
+  renderControls();
 }
 
-function renderBoard(){
-  board.forEach(function(colArr, colIdx){
-    colArr.forEach(function(cellVal, rowIdx){
-      console.log(colIdx, rowIdx)
-    })
-  }) 
+function renderBoard() {
+  board.forEach(function (colArr, colIdx) {
+    colArr.forEach(function (cellVal, rowIdx) {
+      const cellId = `p${rowIdx}${colIdx}`;
+      const cellEl = document.getElementById(cellId);
+      cellEl.style.backgroundColor = COLORS[cellVal]
+    });
+  });
 }
-function renderMessage(){}
-function renderControls(){}
+function renderMessage() {
+
+}
+function renderControls() {}
