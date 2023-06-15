197e827a53e9a5098d37ef339d561e37b835ce3a
Damola Olusakin <olusakinadedamola@gmail.com>
2023-06-15 12:40:52 -0400

    changed board layout

--- app.js
+++ app.js
@@ -18,17 +18,32 @@ let turn
 /*----- functions -----*/
 function init(){
   board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
   ]
   turn = 1
   winner = null
   render()
 }
 
+init()
+
 function render(){
-  
+  renderBoard()
+  renderMessage()
+  renderControls()
 }
+
+function renderBoard(){
+  board.forEach(function(colArr, colIdx){
+    colArr.forEach(function(cellVal, rowIdx){
+      console.log(colIdx, rowIdx)
+    })
+  }) 
+}
+function renderMessage(){}
+function renderControls(){}
